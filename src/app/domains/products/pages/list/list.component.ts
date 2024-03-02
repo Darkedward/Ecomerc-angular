e4b87505caa6c9efc6a69cca267fea8805690e52
edwarde <edu19_unico@outlook.es>
2024-03-02 15:07:13 -0500

    proyecto-finalizado

---
+++ src/app/domains/products/pages/list/list.component.ts
@@ -0,0 +1,66 @@
+
+import { CategoryService } from './../../../shared/services/category.service';
+
+import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
+import {ProductComponent} from '@products/components/product/product.component'
+import { Product } from '@shared/models/product.models';
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { CartService } from '@shared/services/cart.service';
+import { ProductService } from '@shared/services/product.service';
+import { Category } from '@shared/models/category.models';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [ProductComponent, HeaderComponent, RouterLink],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export default class ListComponent {
+
+  products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
+  private cartService = inject(CartService);
+  private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
+  @Input() category_id?:string;
+  
+  ngOnInit(){
+
+    this.getCategories();
+  }
+
+  ngOnChanges(changes:SimpleChanges){
+      this.getProducts();
+
+  }
+  addToCart(product:Product){
+    
+    this.cartService.addToCart(product)
+  }
+
+  private getProducts(){
+    this.productService.getProducts(this.category_id)
+    .subscribe({
+      next: (products)=>{
+        this.products.set(products);
+      },
+      error: ()=>{
+      }
+    })
+  }
+
+
+  private getCategories(){
+    this.categoryService.getAll()
+    .subscribe({
+      next: (data)=>{
+        this.categories.set(data);
+      },
+      error: ()=>{
+      }
+    })
+  }
+
+}
---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,71 @@
+
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+
+  @Input({required:true}) duration =0;
+  @Input({required:true}) message ='';
+  counter = signal(0);
+  // counterRef: number | undefined
+
+  constructor(){
+    //NO ASYNC
+    //before render
+    console.log('constructor')
+    console.log('-'.repeat(10))
+  }
+
+  ngOnChanges(changes:SimpleChanges){
+    
+    //before and during render
+    console.log('ngOnChanges')
+    console.log('-'.repeat(10))
+    console.log(changes);
+    const duration = changes['duration'];
+    if(duration && duration.currentValue !== duration.previousValue){
+      this.doSomething();
+    }
+    
+  }
+
+  ngOnInit(){
+    // after render
+    //una ves
+    //async, then , subs
+    console.log('ngOnInit')
+    console.log('-'.repeat(10))
+    console.log('duration=>', this.duration)
+    console.log('message=>', this.message)
+    // this.counterRef = window.setInterval(()=>{
+    //   console.log('run initerval')
+    //   this.counter.update(statePrev => statePrev +1)
+    // },1000)
+  }
+
+  ngAfterViewInit(){
+  //After render
+  //hijos ya fueron pintados
+  console.log('ngAfterViewInit')
+  console.log('-'.repeat(10))
+  
+  }
+
+  ngOnDestroy(){
+    console.log('ngAfterViewInit')
+    console.log('-'.repeat(10))
+    // window.clearInterval(this.counterRef)
+  }
+
+  doSomething(){
+    console.log('change duration')
+    //async
+  }
+}
+
